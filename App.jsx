e0d23294ddf6a3b3477f57f156f04fbfbbe1a20d
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-06-05 11:35:01 +0200

    Refactor App component to integrate Header, ThoughtForm, and ThoughtList components, removing direct state management and fetching logic.

--- App.jsx
+++ App.jsx
@@ -1,65 +1,17 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { Header } from "./components/Header"
+import { ThoughtForm } from "./components/ThoughtForm"
+import { ThoughtList } from "./components/ThoughtList"
 
-function App() {
-  const [thoughts, setThoughts] = useState([])
-  const [loading, setLoading] = useState(false)
+import "./App.css"
 
-  const fetchThoughts = () => {
-    setLoading(true)
-    fetch("http://localhost:8080/thoughts")
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        }
-        throw new Error('Network response was not ok')
-      })
-      .then(data => {
-        setThoughts(data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }
-
-  useEffect(() => {
-    fetchThoughts()
-  }, [])
-
-  const handleClick = () => {
-    setLoading(true)
-    fetch("http://localhost:8080/thought?color=red")
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        }
-        throw new Error('Network response was not ok')
-      })
-      .then(data => {
-        setThoughts(data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }
-
-  if (loading) {
-    return <p>LOADING...</p>
-  }
-
+export const App = () => { 
   return (
     <>
-      <h1>Happy Thoughts!</h1>
-      <button onClick={handleClick}>Get red thought</button>
-      {thoughts.length && thoughts.map(thought => <p>{thought.name} - {thought.color}</p>)}
+      <Header />
+      <main>
+        <ThoughtForm />
+        <ThoughtList />
+      </main>
     </>
   )
 }
-
-export default App
