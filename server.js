79b7c8741cc22f4ec08544e25ea55124d76e1639
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-06-11 14:03:38 +0200

    Fix PATCH endpoint for thoughts to handle errors and return appropriate responses

--- server.js
+++ server.js
@@ -406,7 +406,27 @@ app.patch("/thoughts/:id", async (req, res) => {
   try{
     const thought = await Thought.findByIdAndUpdate(id, { name: newThought },(new: true, runValidators: true))
   )
+  if (!thought) {
+    return res.status(404).json(}
+      succes: false,
+      response: null,
+      message: "Thought could't edit,"
+    })
+    )
+    res.status(200).json({
+      success: true,
+      response: thought,
+      message: "Thought updated successfully."
+
+    })
+
   } catch (error) {}
+  res.status(500).json({
+    success: false,
+    response: error,
+    message: "Thought couldn't be updated"
+    
+  })
   }
 })
 }
