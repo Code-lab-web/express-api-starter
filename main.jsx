ee19e64942cc67e7f94e951dff4f693c8797684c
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-06-13 18:43:38 +0200

    Refactor main.jsx to use StrictMode and createRoot for improved rendering

--- main.jsx
+++ main.jsx
@@ -1,3 +1,5 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { Header } from "./Header"
 import { ThoughtForm } from "./ThoughtForm"
 import { ThoughtList } from "./ThoughtList"
@@ -5,9 +7,14 @@ import { ColorFilter } from "./ColorFilter"
 import { ThoughtGrid } from "./ThoughtGrid"
 import "./Card.css";
 import "./index.css";
-import "./App.css"
 
+import App from './App.jsx'
 import "./App.css"
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
 export const App = () => { 
   return (
