66923a068e78bebddf535520d64816f5b30a29fd
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-06-13 19:07:01 +0200

    Add additional imports in main.jsx for enhanced functionality and component integration

--- main.jsx
+++ main.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react"
+import { useEffect } from "react"
 import { StrictMode } from 'react'
+
+import { Container, Typography } from "@mui/material";
+
 import { createRoot } from 'react-dom/client'
 import { Header } from "./Header"
 import { ThoughtForm } from "./ThoughtForm"
 import { ThoughtList } from "./ThoughtList"
 import { ColorFilter } from "./ColorFilter"
 import { ThoughtGrid } from "./ThoughtGrid"
+import { LoadingSpinner } from "./LoadingSpinner";
+import { LoginForm } from "./LoginForm";
+import { SignupForm } from "./SignupForm";
+import { EditButton } from "./EditButton"
 import "./Card.css";
 import "./index.css";
+import "./App.css"
 
 import App from './App.jsx'
 import "./App.css"
