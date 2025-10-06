b41442a14a26674ba30d32b4854a64a7679377fd
your_username <your_email@example.com>
2025-10-06 08:12:45 +0000

    git commit -m "Code-lab-web"

--- main.jsx
+++ main.jsx
@@ -1,24 +1,6 @@
-import { useState } from "react"
-import { useEffect } from "react"
 import { StrictMode } from 'react'
 
-import { Container, Typography } from "@mui/material";
-
 import { createRoot } from 'react-dom/client'
-import { Header } from "./Header"
-import { ThoughtForm } from "./ThoughtForm"
-import { ThoughtList } from "./ThoughtList"
-import { ColorFilter } from "./ColorFilter"
-import { ThoughtGrid } from "./ThoughtGrid"
-import { LoadingSpinner } from "./LoadingSpinner";
-import { LoginForm } from "./LoginForm";
-import { SignupForm } from "./SignupForm";
-import { EditButton } from "./EditButton"
-import { Form } from "./Form";
-import { Card } from "./Card";
-import { Index } from "./Index";
-import { Css } from "./Css";
-import { Animation } from "./Animation";
 
 import App from './App.jsx'
 import "./App.css"
@@ -27,15 +9,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-
-export const App = () => { 
-  return (
-    <>
-      <Header />
-      <main>
-        <ThoughtForm />
-        <ThoughtList />
-      </main>
-    </>
-  )
-}
